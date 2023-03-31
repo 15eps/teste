47242775e5550d8a8fbaf4605d5c8b4d19bccbdc
15eps <edu150120@gmail.com>
2023-03-31 10:28:39 -0300

    2 files modified

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 const blockedContent = document.querySelector('main');
 const unBlockcontent = document.getElementById('unblockButton');
 const currentDPI = document.getElementById('current-dpi');
+const sensiInputs = document.querySelectorAll('input[type=range]');
+const sensiInputsValues = document.querySelectorAll('.sensi-value');
 
-window.onload = ()=>{
+window.onload = () => {
   currentDPI.textContent = screen.width - 1;
+  handleInputValue();
+};
+
+function changeValueSensi(value, position) {
+  sensiInputsValues[position].innerHTML = value;
 }
 
-unBlockcontent.addEventListener('click', ()=>{
-  blockedContent.removeAttribute('hidden')
-})
+function handleInputValue() {
+  sensiInputs.forEach((sensiInput, index) => {
+    changeValueSensi(sensiInput.value, index);
+    sensiInput.addEventListener('input', (e) => {
+      changeValueSensi(e.target.value, index);
+    });
+  });
+}
+
+unBlockcontent.addEventListener('click', () => {
+  blockedContent.removeAttribute('hidden');
+});
