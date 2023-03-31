76c3dc782d11cb3e5134e94540eee532445b3ea6
15eps <edu150120@gmail.com>
2023-03-31 08:22:28 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# teste
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-ezwi1b)
---
+++ script.js
@@ -0,0 +1,41 @@
+const formInputs = document.querySelectorAll('form div span');
+const showMessage = document.querySelector('.message');
+const rangeValues = document.querySelectorAll('input[type=range]');
+
+const messages = {
+  media: 'Sua sensi é média',
+  rapida: 'Sua sensi é rápida',
+  lenta: 'Sua sensi é lenta',
+};
+const pixels = Math.sqrt(window.outerHeight ** 2 + window.screen.width ** 2);
+
+function changeMessage() {
+  rangeValues[0].value > 85 && (showMessage.innerHTML = messages.rapida);
+  rangeValues[0].value <= 85 && (showMessage.innerHTML = messages.media);
+  rangeValues[0].value < 60 &&
+    (showMessage.innerHTML = messages.lenta + pixels / (pixels * 0.0104));
+}
+
+changeMessage();
+
+rangeValues.forEach((rangeValue, index) => {
+  formInputs[index].innerHTML = rangeValue.value;
+
+  rangeValue.addEventListener('input', (e) => {
+    formInputs[index].innerHTML = e.target.value;
+    changeMessage();
+  });
+});
+
+function myFunction() {
+  var dpi_x = document.getElementById('dpi').offsetWidth;
+  var dpi_y = document.getElementById('dpi').offsetHeight;
+  var width = screen.width / dpi_x;
+  var height = screen.height / dpi_y;
+  console.log('Width: ' + dpi_y, dpi_x);
+
+  console.log({ width, height });
+}
+myFunction();
+console.log(rangeValues);
+console.log(window);
