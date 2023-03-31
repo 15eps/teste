a4e461990872fa19a8985eb5b7adb0a020ae5337
15eps <edu150120@gmail.com>
2023-03-31 09:51:41 -0300

    3 files modified

--- script.js
+++ script.js
@@ -1,44 +1,11 @@
-const formInputs = document.querySelectorAll('form div span');
-const showMessage = document.querySelector('.message');
-const rangeValues = document.querySelectorAll('input[type=range]');
+const blockedContent = document.querySelector('main');
+const unBlockcontent = document.getElementById('unblockButton');
+const currentDPI = document.getElementById('current-dpi');
 
-const messages = {
-  media: 'Sua sensi é média',
-  rapida: 'Sua sensi é rápida',
-  lenta: 'Sua sensi é lenta',
-};
-const pixels = Math.sqrt(window.outerHeight ** 2 + window.screen.width ** 2);
-
-function changeMessage() {
-  // rangeValues[0].value > 85 && (showMessage.innerHTML = messages.rapida);
-  // rangeValues[0].value <= 85 && (showMessage.innerHTML = messages.media);
-  var dpi_x = document.getElementById('dpi').offsetWidth;
-  var dpi_y = document.getElementById('dpi').offsetHeight;
-  rangeValues[0].value && (showMessage.innerHTML = `dpi_y ${dpi_y}, dpy_x ${dpi_x}, scren W ${screen.width}, H ${screen.height}`);
+window.onload = ()=>{
+  currentDPI.textContent = screen.width - 1;
 }
 
-
-changeMessage();
-
-rangeValues.forEach((rangeValue, index) => {
-  formInputs[index].innerHTML = rangeValue.value;
-
-  rangeValue.addEventListener('input', (e) => {
-    formInputs[index].innerHTML = e.target.value;
-    changeMessage();
-  });
-});
-
-function myFunction() {
-  var dpi_x = document.getElementById('dpi').offsetWidth;
-  var dpi_y = document.getElementById('dpi').offsetHeight;
-  var width = screen.width / dpi_x;
-  var height = screen.height / dpi_y;
-  console.log('Width: ' + dpi_y, dpi_x);
-
-  console.log({ width, height });
-}
-myFunction();
-console.log(rangeValues);
-console.log(window);
-// screen.width + pixels / (pixels * 0.0104)
+unBlockcontent.addEventListener('click', ()=>{
+  blockedContent.removeAttribute('hidden')
+})
